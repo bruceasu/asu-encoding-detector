081efc4a90501da30f94a2a56c85cd2e392085ee
Bruce Asu <bruceasu@163.com>
2019-04-15 23:38:32 +0800

    first commit

---
+++ src/main/java/me/asu/encoding/Constants.java
@@ -0,0 +1,46 @@
+/**
+ * @Project tax-info-collection
+ * @Title DefaultConstants.java
+ * @Package com.pycredit.util
+ * @Description: TODO
+ * @author Suk Honzeon(suhj@pycredit.cn)
+ * @date 2012-7-10 ����6:03:22
+ * @Copyright 2012 pycredit Inc. All rights reserved.
+ * @version V1.0
+ */
+
+package me.asu.encoding;
+
+public class Constants {
+
+    public static final String CHARSET_HZ = "HZ";
+
+    public static final String CHARSET_GB = "GBK";
+
+    public static final String CHARSET_GB2312 = "GB2312";
+
+    public static final String CHARSET_GB18030 = "GB18030";
+
+    public static final String CHARSET_US_ASCII = "US-ASCII";
+
+    public static final String CHARSET_WINDOWS_1252 = "windows-1252";
+
+    public static final String CHARSET_ISO_8859_1 = "iso-8859-1";
+
+    public static final String CHARSET_UTF16 = "UTF-16";
+
+    public static final String CHARSET_UTF16BE = "UTF-16BE";
+
+    public static final String CHARSET_UTF16LE = "UTF-16LE";
+
+    public static final String CHARSET_UTF8 = "UTF-8";
+
+
+    public static final String CHARSET_BIG5 = "BIG5";
+
+
+    public static final String CHARSET_BIG5HKSCS = "BIG5HKSCS";
+
+    public static final String CHARSET_DEFAULT = System.getProperty("file.encoding", "GB18030");
+
+}
